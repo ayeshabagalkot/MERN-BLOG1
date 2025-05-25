bd048d3b5527851d23e3b781ab4aeaf7175f23b0
Ayesha <ayeshabagalkot@gmail.com>
2025-05-25 16:17:23 +0530

    Update firebase.js

--- client/src/helpers/firebase.js
+++ client/src/helpers/firebase.js
@@ -5,15 +5,6 @@ import { getEvn } from "./getEnv";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyChzW-avJ6EK8kv8EjDN3WShv_iv-UDe0Y",
-  authDomain: "blog-e7b0d.firebaseapp.com",
-  projectId: "blog-e7b0d",
-  storageBucket: "blog-e7b0d.firebasestorage.app",
-  messagingSenderId: "894240222841",
-  appId: "1:894240222841:web:897bacfc941e50a6b36ea8",
-  measurementId: "G-C9KM8B8FKY"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
